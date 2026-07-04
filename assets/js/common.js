49dd9bedbd579c04e00aafd3b133cd4f5d0d5619
wenxiang.zhou <mss_xiumian@sina.com>
2026-07-04 13:05:54 +0800

    优化

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,34 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Remove the base tag added by EZOIC ADS in the head
   const baseTag = document.querySelector('base');
   if (baseTag) {
     baseTag.remove();
   }
 });
 
-/**
- * 获取Cookie
- *
- * @param name
- * @returns {string|null}
- */
 function publicGetCookie(name) {
   var reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
   var arr = document.cookie.match(reg);
   return arr ? unescape(arr[2]) : null;
 }
 
-/**
- * 设置Cookie
- *
- * @param name
- * @param value
- * @param timeoutM
- */
 function publicSetCookie(name, value, timeoutM = 0) {
   if (timeoutM) {
     var now = new Date();
-    //设置时间
     now.setMinutes(now.getMinutes() + timeoutM);
 
     document.cookie = `${name}=${value}; expires=${now.toUTCString()}; path=/`;
@@ -37,9 +22,6 @@ function publicSetCookie(name, value, timeoutM = 0) {
   }
 }
 
-/**
- * 处理联盟计划Cookie
- */
 function publicProcessAffiliateProgramCookie() {
   if (window && window.location) {
     const searchParams = new URLSearchParams(window.location.search);
@@ -62,19 +44,6 @@ function publicProcessAffiliateProgramCookie() {
   }
 }
 
-// 非Iframe引入状态下逻辑
 if (window.top === window.self) {
-  // 处理联盟计划Cookie
   publicProcessAffiliateProgramCookie();
-
-  // Tawk
-  var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-  (function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/6384dc50b0d6371309d18b87/1givh48mt';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-  })();
 }
