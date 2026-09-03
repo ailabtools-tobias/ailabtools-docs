e0e1c52a3c254cd013c55a5233d8c629ad6f526a
wenxiang.zhou <mss_xiumian@sina.com>
2026-09-03 20:30:39 +0800

    修复clarity问题

--- assets/js/clarity.js
+++ assets/js/clarity.js
@@ -2,4 +2,4 @@
     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "uenwz3gkwu");
+})(window, document, "clarity", "script", "ycgow2oc02");
